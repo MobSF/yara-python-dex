49cd367b803d18a094778e97bc4d6b1088dfd5b4
Ajin Abraham <ajin25@gmail.com>
2021-01-21 21:32:54 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -312,7 +312,7 @@ with open('yara-python/README.rst', 'r', 'utf-8') as f:
 
 setup(
     name='yara-python-dex',
-    version='1.0.1',
+    version='1.0.2',
     description='Python interface for YARA',
     long_description=readme,
     license='Apache 2.0',
