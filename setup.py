70496cab391e10eee9d0aa1cbf6ba1c0fdd1ba18
Ajin Abraham <ajin25@gmail.com>
2025-08-30 21:33:28 -0700

    Workflow update

--- setup.py
+++ setup.py
@@ -312,7 +312,7 @@ with open('yara-python/README.rst', 'r', 'utf-8') as f:
 
 setup(
     name='yara-python-dex',
-    version='1.0.8',
+    version='1.0.9',
     description='Python interface for YARA',
     long_description=readme,
     long_description_content_type='text/markdown',
