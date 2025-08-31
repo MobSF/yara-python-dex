6c3ae224710b6b57306a9cbc96e1651a9f23141a
Ajin Abraham <ajin25@gmail.com>
2025-08-30 20:40:05 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -312,7 +312,7 @@ with open('yara-python/README.rst', 'r', 'utf-8') as f:
 
 setup(
     name='yara-python-dex',
-    version='1.0.7',
+    version='1.0.8',
     description='Python interface for YARA',
     long_description=readme,
     long_description_content_type='text/markdown',
